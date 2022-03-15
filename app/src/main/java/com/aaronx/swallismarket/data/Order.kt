400a1473592369cb38c3f04d1699c24bf2b904ef
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-03-15 05:44:00 +0100

    add some comments about field for each dataclass

--- app/src/main/java/com/aaronx/swallismarket/data/Order.kt
+++ app/src/main/java/com/aaronx/swallismarket/data/Order.kt
@@ -4,3 +4,11 @@ data class Order(
     var orderDate: String = "",
     var state: String = ""
 )
+/**
+ * + idUser String
+ * + nameProduct String
+ * + pictureProduct String
+ * + priceProduct String
+ * + quantity String
+ * +
+ * **/
--- app/src/main/java/com/aaronx/swallismarket/data/Product.kt
+++ app/src/main/java/com/aaronx/swallismarket/data/Product.kt
@@ -5,3 +5,13 @@ data class Product(
     var price: Double = 0.0,
     var picture: String = "",
 )
+/**
+ * add field :
+ * ingredients String
+ * description String
+ * idUser String
+ * price convert to String not Double
+ *
+ * and list of orders kima list of product with status we khlass
+ *
+ * */
